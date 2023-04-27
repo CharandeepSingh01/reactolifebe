5fa9bf390eed5cecede8ab5622b3bf1592e82727
CharandeepSingh01 <login.charandeep@gmail.com>
2023-04-27 16:44:14 +0530

    atlas commit

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGOBD_URL: "mongodb+srv://charandeep:1234567890@reactolife.zex6ihk.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET: "hafsfffgg72846285jjjkh54646467hh"
+}
